24f06c50f062708138d7c37412b328da50b81e49
Siva <sivapitts@yahoo.com>
2020-12-08 21:11:22 +0800

    Video9

---
+++ week05/scripts/video.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    let video = document.getElementById("myVideo");
+
+    let playBtn = document.getElementById("playBtn");
+    playBtn.addEventListener("click", function () {
+        video.play();
+    });
+
+    let pauseBtn = document.getElementById("pauseBtn");
+    pauseBtn.addEventListener("click", function () {
+        video.pause();
+    });    
+
+    let freezeBtn = document.getElementById("mousedown", function () {
+        video.pause();
+    });
+    freezeBtn.addEventListener("mouseup", function () {
+        video.play();
+    });
+};
+
+let seekBar = document.getElementById("seekBar");
+seekBar.addEventListener("change", function() {
+    let time = video.duration * (seekBar.value / 100); 
+    video.currentTime = time; 
+});
+seekBar.addEventListener("timeupdate", function() {
+    let value = (100 /video.duration) * video.currentTome;
+    seekBar.value - value;
+});   
