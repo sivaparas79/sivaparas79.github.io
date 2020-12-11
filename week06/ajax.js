a4341ccf1a6b412c22d2b3aa975f813c37017df6
Siva <sivapitts@yahoo.com>
2020-12-11 20:29:42 +0800

    ajax2

---
+++ week06/ajax.js
@@ -0,0 +1,21 @@
+function getrandomUser(){
+    let xhttp = new XMLHttpRequest(); //create request obj
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let data = JSON.parse(this,response);
+            let elFirstName=document.getElementById("firstName");
+            let elLastName=document.getElementById("lastName");
+            let elJasonResult=document.getElementById("jsonResult");
+            let elUSerImage=document.getElementById("userImage");
+            let elPhone=document.getElementById("phone");
+            let elAddress=document.getElementById("address");
+            let elEmail=document.getElementById("email");
+
+            elJasonResult.innerHTML = this.response;
+        }
+    }
+
+    xhttp.open("GET", "https://randomuser.me/api/",true);
+    xhttp.send();
+}
