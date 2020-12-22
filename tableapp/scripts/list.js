b1524f4e212d51ba82a075d5d30994448bc99102
Siva <sivapitts@yahoo.com>
2020-12-22 20:25:17 +0800

    commit on main

---
+++ tableapp/scripts/list.js
@@ -0,0 +1,25 @@
+function GetBookings() {
+    let url = 'https://api.sheety.co/e8e5c93834133be34d5302cea5c6ceda/bookingApp/sheet1';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.sheet1);
+            let bookingList = document.getElementById("bookingList");
+            bookingList.innerHTML = "";
+
+            let jsonDiv = document.getElementById("json");
+            jsonDiv.innerHTMl = "";
+            jsonDiv.innerHTML = json.sheet1;
+
+            for (let i = 0; i < json.sheet1.length; i++) {
+                let gName = json.sheet1[i].name;
+                let gEmail = json.sheet1[i].email;
+                let gPax = json.sheet1[i].pax;
+                let gId = json.sheet1[i].id;
+
+                bookingList.innerHTML += gId + "-" + gName + ", " +
+                    gEmail + ", pax:" + gPax + "<br>";
+            }
+        });
+}
