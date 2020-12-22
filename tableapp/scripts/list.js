8f8bb8be6937112828f57011f617db6accb7fe1e
Siva <sivapitts@yahoo.com>
2020-12-22 20:50:56 +0800

    commint at main

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -5,21 +5,41 @@ function GetBookings() {
         .then(json => {
             // Do something with the data
             console.log(json.sheet1);
-            let bookingList = document.getElementById("bookingList");
-            bookingList.innerHTML = "";
-
-            let jsonDiv = document.getElementById("json");
-            jsonDiv.innerHTMl = "";
-            jsonDiv.innerHTML = json.sheet1;
+            let bookingList = document.getElementById("booking-list");
+            let bookingIds = [];
 
             for (let i = 0; i < json.sheet1.length; i++) {
                 let gName = json.sheet1[i].name;
                 let gEmail = json.sheet1[i].email;
                 let gPax = json.sheet1[i].pax;
                 let gId = json.sheet1[i].id;
+                let gRemarks = json.bookings[i].remarks;
+                let buttonId = "delete" + gId;
 
-                bookingList.innerHTML += gId + "-" + gName + ", " +
-                    gEmail + ", pax:" + gPax + "<br>";
+                let row = bookingList.insertRow(bookingList.rows.length);
+                row.insertCell(0).innerHTML = gId;
+                row.insertCell(1).innerHTML = gName;
+                row.insertCell(2).innerHTML = gEmail;
+                row.insertCell(3).innerHTML = gPax;
+                row.insertCell(4).innerHTML = gRemarks;
+                row.insertCell(1).innerHTML = "<button id='" +
+                    buttonId + "' class='btn btn-danger'>Delete</button>";
+
+                bookingIds.push(buttonId);
+            }
+
+            for (let j = 0; j < bookingIds.length; j++) {
+                let el = document.getElementById(bookingIds[j]);
+                el.addEventListener("click", function () {
+                    let theId = bookingIds[j].replace("delete", "");
+                    alert(theId);
+                });
             }
         });
 }
+
+window.addEventListener("load", function () {
+    document.getElementById("refreshList").addEventListener("click", function () {
+        GetBookings();
+    });
+})
