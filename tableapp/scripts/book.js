0c9e6b775c8bcc20071cf612c5f6bb479054e996
Siva <sivapitts@yahoo.com>
2020-12-22 20:15:35 +0800

    commit on main

---
+++ tableapp/scripts/book.js
@@ -0,0 +1,33 @@
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
+    let url = "";
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax,
+            remarks: guestRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!");
+        });
+}
+
+window.addEventListener("load", function () {
+    document.getElementById("bookNow").addEventListener("click", function () {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPax = document.getElementById("userPax").value;
+        let userRemarks = document.getElementById("userRemarks").value;
+
+        BookNow(userName, userEmial, userPax, userRemarks);
+    });
+});
